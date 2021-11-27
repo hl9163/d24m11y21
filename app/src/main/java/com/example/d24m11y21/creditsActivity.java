fd0bddb03398055e70b933f43c5416dcd1e2c252
HAREL <hl9163@bs.amalnet.k12.il>
2021-11-27 11:32:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/d24m11y21/creditsActivity.java
@@ -0,0 +1,31 @@
+package com.example.d24m11y21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * @author		Harel Leibovich <address @ example.com>
+ * @version	1.0 (current version number of program)
+ * @since		27/11/2021 (the date of the package the class was added)
+ * short description
+ */
+
+public class creditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+    /**
+     * return the the first activity.
+     * <p>
+     *
+     */
+    public void returnFistAc(View view) {
+        finish();
+    }
+}
